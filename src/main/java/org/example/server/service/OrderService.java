b34aa8809cca219e28b17d25ba27eed8e60ccd18
EnU098 <ergashevnodir1998@gmail.com>
2023-07-17 22:37:26 +0500

    create service2

---
+++ src/main/java/org/example/server/service/OrderService.java
@@ -0,0 +1,61 @@
+package org.example.server.service;
+
+import com.company.server.model.Order;
+import com.google.gson.reflect.TypeToken;
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.UUID;
+
+public class OrderService implements BaseService<Order> {
+    private Path path = Path.of("src/main/resources/order.json");
+
+    @Override
+    public Order add(Order order) {
+        List<Order> orderList = readFile();
+        orderList.add(order);
+        writeFile(orderList);
+        return order;
+    }
+    @Override
+    public List<Order> getAll() {
+        return readFile();
+    }
+
+    @Override
+    public Order getById(UUID id) {
+        return readFile().stream()
+                .parallel()
+                .filter(order -> order.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        List<Order> orderList = readFile();
+        orderList.removeIf(order -> order.getId().equals(id));
+        writeFile(orderList);
+    }
+
+    @Override
+    public Order update(Order order) {
+        delete(order.getId());
+        add(order);
+        return order;
+    }
+
+    @SneakyThrows
+    @Override
+    public void writeFile(List<Order> list) {
+        Files.writeString(path, gson.toJson(list), StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    @SneakyThrows
+    @Override
+    public List<Order> readFile() {
+        return gson.fromJson(Files.readString(path), new TypeToken<List<Order>>() {
+        }.getType());
+    }
+}
