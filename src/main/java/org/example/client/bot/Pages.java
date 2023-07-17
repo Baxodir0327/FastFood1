61902f5b9787973558ca7f02d02831f16f063ee7
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-07-17 23:34:35 +0500

    added product service

---
+++ src/main/java/org/example/client/bot/Pages.java
@@ -0,0 +1,16 @@
+package org.example.client.bot;
+
+import org.example.server.service.CategoryService;
+import org.example.server.service.CreateButtonService;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+
+import java.util.List;
+
+public class Pages {
+    private static CategoryService categoryService = new CategoryService();
+
+    public ReplyKeyboard mainPage(CreateButtonService createButtonService, boolean admin) {
+        List<String> mainButtons = categoryService.getMainPageCategory(admin);
+        return createButtonService.createReplyButton(mainButtons, false);
+    }
+}
