c8012b8b1f2654865f98a60638b5658f6d5e06fc
Baxodir <bpardaboyev2000@gmail.com>
2023-07-17 23:06:51 +0500

    commit from bot

--- src/main/java/org/example/client/bot/BotConstants.java
+++ src/main/java/org/example/client/bot/BotConstants.java
@@ -1,4 +1,16 @@
 package org.example.client.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BotConstants {
+    public static final String USERNAME = "https://t.me/bildikbot";
+    public static final String TOKEN = "6032607553:AAG93Tcwe9I4x3J_H30i8bYHq9UfNzdpmVE";
+    public static final String FIRST_MSG = """
+            Assalomu Alaykum! Bizning botdan foydalanish uchun ro'yxatdan o'ting.
+            ism familyangizni kiriting:
+            Hello! First you need to register to use our bot
+            Enter full name:
+            """;
+    public static final List<String> ADMIN_NUMBERS = new ArrayList<>(List.of("+998931419445", "+998911638343"));
 }
